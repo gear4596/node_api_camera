8ab6df81061aadd46f8e3f77e2520b25e3321847
gear4596 <65010912614@msu.ac.th>
2024-10-15 18:03:21 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('nodeapidb', 'postgres', '12345678', {
+const sequelize = new Sequelize('nodeapidb', 'postgres', '0930121453gg2545', {
 host: 'localhost',
 dialect: 'postgres',
 });
